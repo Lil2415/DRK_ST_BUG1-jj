b2849b1a49b747eae3f06a98932dfa191f749d16
Lil2415 <136098662+Lil2415@users.noreply.github.com>
2024-09-09 16:18:44 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,13 +2,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "50931461936"
+global.ownernomer = "254706697688"
 global.ownername = "🌹⃟⃢👑 ĐÆ𝚪𝐊༒𝙎-𝙏𝞢𝞜🌹⃟⃢👑"
 global.ytname = "DRK_ST_Tech"
 global.socialm = "GitHub: DRK-S-TEN"
 global.location = "America, Haiti, Port-au-Prince"
 
-global.ownernumber = '50931461936'  //creator number
+global.ownernumber = '254706697688'  //creator number
 global.ownername = '𝙎-𝙏𝞢𝞜' //owner name
 global.botname = 'DRK_ST_BUG' //name of the bot
 
